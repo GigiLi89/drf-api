a64b68a4356299f6b65c4616c4c74b12dd3694e5
GigiLi89 <li.gigi89@gmail.com>
2024-06-05 08:47:58 +0000

    Added profile detail views

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,4 +5,5 @@ from profiles import views
 # ProfileList is a class list so therefore we need to call it with as_view
 urlpatterns = [
     path('profiles/', views.ProfileList.as_view()),
+    path('profiles/<int:pk>/', views.ProfileDetail.as_view())
 ]
