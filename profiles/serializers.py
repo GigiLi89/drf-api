234f2a72fcd9a860c8a104df8b5e953b81e61171
GigiLi89 <li.gigi89@gmail.com>
2024-06-05 09:38:31 +0000

    Added login and logout views + permissions, is_owner

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,6 +4,12 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner =serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
 
     # Can also be written with __all__ but it's better to specify
     # as below if you would like to exclude something in the future.
@@ -11,5 +17,5 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image'
+            'content', 'image', 'is_owner'
         ]
